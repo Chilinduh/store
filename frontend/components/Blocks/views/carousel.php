bd6fcc74c7fcf4f4b46ddc0f49e7b97c73968a64
s.mezentsev <kurt_dc@mail.ru>
2026-03-17 18:14:49 +0300

    fix banner carusel

--- frontend/components/Blocks/views/carousel.php
+++ frontend/components/Blocks/views/carousel.php
@@ -3,12 +3,20 @@
   <div class="container">
     <div class="block-slideshow__carousel">
       <div class="owl-carousel">
-        <?php foreach ($model as $item) { ?>
+        <?php
+
+        echo "<pre>";
+        print_r($model);
+        echo "</pre>";
+        die;
+
+
+        foreach ($model as $item) { ?>
           <a class="block-slideshow__item" href="<?= $model->link??'#' ?>">
             <span class="block-slideshow__item-image block-slideshow__item-image--desktop" style="background-image: url('<?= $item->files->original??''?>')"></span>
             <span class="block-slideshow__item-image block-slideshow__item-image--mobile" style="background-image: url('<?= $item->files->original??''?>')"></span>
             <span class="block-slideshow__item-offer"></span>
-            <span class="block-slideshow__item-title"><?= $model->title ?></span>
+            <span class="block-slideshow__item-title"><?= $model->title??'' ?></span>
             <span class="block-slideshow__item-details"><?= $model->announce ?></span>
           </a>
         <?php } ?>
