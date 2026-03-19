42840ce13086a2eac8d162c602a508511b4bb2a0
s.mezentsev <kurt_dc@mail.ru>
2026-03-19 07:39:33 +0300

    add filter fix

--- frontend/controllers/CatalogController.php
+++ frontend/controllers/CatalogController.php
@@ -300,6 +300,12 @@ class CatalogController extends Controller
 
         $filters = $this->filterService->getFilters($category['id'], $params);
 
+        echo "<pre>";
+        print_r($filters);
+        echo "</pre>";
+        die;
+
+
         $brands = ArrayHelper::toArray(Products::getProductsBrands($params['category_id']??''));
         if($brands) {
           $filters[] = [
