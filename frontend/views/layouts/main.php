5757dd41cbfc621afca22a9d3d1811ffe46bb713
s.mezentsev <kurt_dc@mail.ru>
2026-04-03 19:50:41 +0300

    fix main slider

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -49,7 +49,19 @@ $this->beginPage();
     $this->title = Yii::$app->params['meta']['title']??'';
   }
   ?>
+  <!-- Yandex.Metrika counter -->
+  <script type="text/javascript">
+    (function(m,e,t,r,i,k,a){
+      m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+      m[i].l=1*new Date();
+      for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+      k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+    })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=108384459', 'ym');
 
+    ym(108384459, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
+  </script>
+  <noscript><div><img src="https://mc.yandex.ru/watch/108384459" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+  <!-- /Yandex.Metrika counter -->
   <?php $this->head() ?>
 
   <link rel="stylesheet" href="/css/blue/style.header-classic-variant-three.css" media="(min-width: 1200px)">
