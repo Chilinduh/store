d4cac4d376fe3b1fd3f54ca08d32c09f64459bfe
Sergey <kurt_dc@mail.ru>
2026-05-15 17:55:57 +0300

    fix

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -48,19 +48,10 @@ $this->beginPage();
     //Yii::$app->view->registerMetaTag(['property' => 'og:url', 'content' => '']); //canonical URL
     $this->title = Yii::$app->params['meta']['title']??'';
   }
-  ?>
-  <!-- Yandex.Metrika counter -->
-  <script type="text/javascript">
-    (function(m,e,t,r,i,k,a){
-      m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-      m[i].l=1*new Date();
-      for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-      k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-    })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=108384459', 'ym');
 
-    ym(108384459, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
-  </script>
-  <noscript><div><img src="https://mc.yandex.ru/watch/108384459" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+  echo $this->title = Yii::$app->params['yandex_metrica']??'';
+  ?>
+
   <!-- /Yandex.Metrika counter -->
   <?php $this->head() ?>
 
