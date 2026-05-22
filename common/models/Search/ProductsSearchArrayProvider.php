df3c35129cfad8f6b75d57e1f66f18d9e285ca4c
Sergey <kurt_dc@mail.ru>
2026-05-22 21:25:00 +0300

    add products

--- common/models/Search/ProductsSearchArrayProvider.php
+++ common/models/Search/ProductsSearchArrayProvider.php
@@ -116,11 +116,6 @@ class ProductsSearchArrayProvider extends Model
       $catalogService = new CatalogFilterService();
       $filterParams = $catalogService->getFiltersProducts($params);
 
-      echo '<pre>';
-print_r($filterParams);
-echo '</pre>';
-die;
-
       $params['productsIds'] = $filterParams['products'];
 
       if(is_array($filterParams['filtersMain']) && count($filterParams['filtersMain'])) {
