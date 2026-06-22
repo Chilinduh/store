42f4a8b0845d6bd090d989d4484029f7ba23287d
Sergey <kurt_dc@mail.ru>
2026-06-22 13:13:07 +0300

    add tags to products

--- backend/views/products/all.php
+++ backend/views/products/all.php
@@ -10,8 +10,24 @@ use app\components\PanelWidget;
 ]);
 ?>
 
+
 <?php
 echo PanelWidget::start();
+
+?>
+
+
+Сделать описание карточки товара 
+
+1) краткое описание товара до 500 символов (не использовать слова - доставка по россии)<br>
+2) полное описание товара (не использовать таблицы, от 3000 символов)<br>
+3) описание для ключевых слов title, keywords, description (от 150 до 250 символов), в ключевых словах использовать слова: цена, доставка по России<br>
+<br><BR>
+Список товаров:
+
+<br><BR>
+
+<?php
 
 foreach($products as $item) {
 //'.(strlen($model->announce)).'/'.(strlen($model->description)).'/'.(strlen($model->tag_title)).'/'.(strlen($model->tag_keywords)).'/'.(strlen($model->tag_description)).'
